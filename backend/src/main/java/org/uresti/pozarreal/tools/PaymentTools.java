0d3dbacfe47636b4d4d038265f6aac75859ca6a2
josee <joseedilberto367@outlook.com>
2022-02-04 11:59:56 -0600

    inject pozarrealConfig

--- backend/src/main/java/org/uresti/pozarreal/tools/PaymentTools.java
+++ backend/src/main/java/org/uresti/pozarreal/tools/PaymentTools.java
@@ -12,60 +12,53 @@ import java.util.List;
 import static org.uresti.pozarreal.model.PaymentSubConcept.*;
 
 public class PaymentTools {
-    public static void setYearPayments(House house, List<Payment> payments) {
-        String[] twoMonthsPaymentIds = {
-                MAINTENANCE_BIM_1,
-                MAINTENANCE_BIM_2,
-                MAINTENANCE_BIM_3,
-                MAINTENANCE_BIM_4,
-                MAINTENANCE_BIM_5,
-                MAINTENANCE_BIM_6
-        };
 
-        ArrayList<PaymentByConcept> paymentInfo = new ArrayList<>();
+	public static void setYearPayments(PozarrealConfig pozarrealConfig, House house, List<Payment> payments) {
+		String[] twoMonthsPaymentIds = {
+				MAINTENANCE_BIM_1,
+				MAINTENANCE_BIM_2,
+				MAINTENANCE_BIM_3,
+				MAINTENANCE_BIM_4,
+				MAINTENANCE_BIM_5,
+				MAINTENANCE_BIM_6
+		};
 
-        paymentInfo.add(new PaymentByConcept());
-        paymentInfo.add(new PaymentByConcept());
-        paymentInfo.add(new PaymentByConcept());
-        paymentInfo.add(new PaymentByConcept());
-        paymentInfo.add(new PaymentByConcept());
-        paymentInfo.add(new PaymentByConcept());
+		ArrayList<PaymentByConcept> paymentInfo = new ArrayList<>();
 
-        house.setTwoMonthsPayments(paymentInfo);
+		paymentInfo.add(new PaymentByConcept());
+		paymentInfo.add(new PaymentByConcept());
+		paymentInfo.add(new PaymentByConcept());
+		paymentInfo.add(new PaymentByConcept());
+		paymentInfo.add(new PaymentByConcept());
+		paymentInfo.add(new PaymentByConcept());
 
-        PozarrealConfig pozarrealConfig = new PozarrealConfig();
+		house.setTwoMonthsPayments(paymentInfo);
 
-        FeeConfig feeConfig = new FeeConfig();
+		FeeConfig feeConfig = pozarrealConfig.getFees();
 
-        feeConfig.setYearlyMaintenanceFee(4560.0);
-        feeConfig.setBiMonthlyMaintenanceFee(800.0);
-        feeConfig.setParkingPenFee(1500.0);
-
-        pozarrealConfig.setFees(feeConfig);
-
-        for (Payment payment : payments) {
-            if (MAINTENANCE_ANNUITY.equals(payment.getPaymentSubConceptId())) {
-                for (PaymentByConcept paymentByConcept : paymentInfo) {
-                    paymentByConcept.setId(payment.getId());
-                    paymentByConcept.setComplete(true);
-                    paymentByConcept.setAmount(payment.getAmount());
-                    paymentByConcept.setValidated(payment.isValidated());
-                    paymentByConcept.setConflict(payment.isConflict());
-                }
-                break;
-            } else {
-                for (int i = 0; i < twoMonthsPaymentIds.length; i++) {
-                    if (twoMonthsPaymentIds[i].equals(payment.getPaymentSubConceptId())) {
-                        paymentInfo.get(i).setId(payment.getId());
-                        paymentInfo.get(i).setAmount(paymentInfo.get(i).getAmount() + payment.getAmount());
-                        paymentInfo.get(i).setComplete(paymentInfo.get(i).getAmount() >= feeConfig.getBiMonthlyMaintenanceFee());
-                        paymentInfo.get(i).setValidated(payment.isValidated());
-                        paymentInfo.get(i).setConflict(payment.isConflict());
-                        break;
-                    }
-                }
-            }
-        }
-    }
+		for (Payment payment : payments) {
+			if (MAINTENANCE_ANNUITY.equals(payment.getPaymentSubConceptId())) {
+				for (PaymentByConcept paymentByConcept : paymentInfo) {
+					paymentByConcept.setId(payment.getId());
+					paymentByConcept.setComplete(true);
+					paymentByConcept.setAmount(payment.getAmount());
+					paymentByConcept.setValidated(payment.isValidated());
+					paymentByConcept.setConflict(payment.isConflict());
+				}
+				break;
+			} else {
+				for (int i = 0; i < twoMonthsPaymentIds.length; i++) {
+					if (twoMonthsPaymentIds[i].equals(payment.getPaymentSubConceptId())) {
+						paymentInfo.get(i).setId(payment.getId());
+						paymentInfo.get(i).setAmount(paymentInfo.get(i).getAmount() + payment.getAmount());
+						paymentInfo.get(i).setComplete(paymentInfo.get(i).getAmount() >= feeConfig.getBiMonthlyMaintenanceFee());
+						paymentInfo.get(i).setValidated(payment.isValidated());
+						paymentInfo.get(i).setConflict(payment.isConflict());
+						break;
+					}
+				}
+			}
+		}
+	}
 
 }
